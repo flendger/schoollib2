67d85f01f50fa6d40b7e84e0a4bbf518a4f04cdb
flendger <kiru.ee@yandex.ru>
2021-01-30 18:55:37 +0300

    Initial commit

---
+++ src/main/java/ru/flendger/schoollib2/Schoollib2Application.java
@@ -0,0 +1,13 @@
+package ru.flendger.schoollib2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schoollib2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Schoollib2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/flendger/schoollib2/Schoollib2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.flendger.schoollib2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Schoollib2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
